b9af0424a0fd144bca3736acb014d8863ec21f80
N3keT <n3ket1@yandex.ru>
2023-02-03 18:10:01 +0400

    сделать проект 1.1

--- app.js
+++ app.js
@@ -2,7 +2,16 @@ const express = require('express');
 const mongoose = require('mongoose');
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
-const { INCORRECT_DATA_ERROR, NOT_FOUND_ERROR, DEFAULT_ERROR } = require('./utils/errors');
+const { NOT_FOUND_ERROR } = require('./utils/errors');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -11,6 +20,8 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
 
+app.use(limiter);
+app.use(helmet());
 app.use(express.json());
 app.use((req, res, next) => {
   req.user = {
