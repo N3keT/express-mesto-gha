43ca5630e9b4a3aaf6d20552e0aa162d6a0e860a
N3keT <n3ket1@yandex.ru>
2023-02-03 18:59:21 +0400

    сделать проект 1.1

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ const mongoose = require('mongoose');
 // const cardsRouter = require('./routes/cards');
 const { NOT_FOUND_ERROR } = require('./utils/errors');
 const helmet = require('helmet');
-// const rateLimit = require('express-rate-limit');
+const rateLimit = require('express-rate-limit');
 
-// const limiter = rateLimit({
-//   windowMs: 15 * 60 * 1000,
-//   max: 100,
-//   standardHeaders: true,
-//   legacyHeaders: false,
-// });
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -20,7 +20,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
 
-// app.use(limiter);
+app.use(limiter);
 app.use(helmet());
 app.use(express.json());
 app.use((req, res, next) => {
